2217fe899a1200738c608f2fa56e5eb42a560cd4
Ellap.delta <ellap.delta@gmail.com>
2023-08-24 20:43:29 +0300

    24/8 kml func (finish)

--- app/routes/Upload_Route.py
+++ app/routes/Upload_Route.py
@@ -4,7 +4,7 @@ import pandas as pd
 from fastapi import APIRouter
 from starlette.responses import JSONResponse
 
-from app.utils.Azure_connaction import get_blob_data, upload_blob_to_azure
+from app.utils.Azure_connaction import get_blob_data, upload_to_azure, get_kml_data
 from app.utils.Help_function import df_validation, tgv_2_xlsx
 from app.Validation.BM_validation import FileInfo
 
@@ -20,26 +20,31 @@ def process_file(excel: FileInfo):
 
         try:
             blob_name, local_blob_csv_data = get_blob_data(excel.accunt_name, excel.container_name, excel.folder_name)
+            local_kml_file = get_kml_data(excel.accunt_name, excel.container_name, excel.folder_name)
         except Exception as e:
             error_message = "An error occurred while connecting azure, check connection info : " + str(e)
             return JSONResponse(content=error_message, status_code=400)
 
-        new_file_name = df_validation(local_blob_csv_data, blob_name)
+        new_csv_file = df_validation(local_blob_csv_data, blob_name)
 
-        directory, filename = os.path.split(new_file_name)
+        directory, filename = os.path.split(new_csv_file)
         new_filename = os.path.splitext(filename)[0] + ".tgv"
-        new_tgv_path = os.path.join(directory, new_filename)
-        os.rename(new_file_name, new_tgv_path)
+        new_tgv_file = os.path.join(directory, new_filename)
+        os.rename(new_csv_file, new_tgv_file)
 
-        upload_blob_to_azure(excel.accunt_name, excel.container_name, excel.folder_name, new_tgv_path)
+        upload_to_azure(excel.accunt_name, excel.container_name, excel.folder_name, new_tgv_file)
+
+        upload_to_azure(excel.accunt_name, excel.container_name, excel.folder_name, local_kml_file)
+
         os.remove(local_blob_csv_data)
-        os.remove(new_tgv_path)
+        os.remove(new_tgv_file)
+        os.remove(local_kml_file)
 
         return JSONResponse(content={"message": "new file created successfully and was upload to the azure storage",
                                      "azure_account": f'{excel.accunt_name}',
                                      "azure_container": f"{excel.container_name}",
                                      "azure_folder": f"{excel.folder_name}",
-                                     "file_name in a": f'{new_tgv_path}'},
+                                     "file_name in a": f'{new_tgv_file}'},
                             status_code=200)
 
     except ValueError as e:
--- app/utils/Azure_connaction.py
+++ app/utils/Azure_connaction.py
@@ -1,3 +1,5 @@
+import os
+
 from starlette.responses import JSONResponse
 
 from app.utils.Help_function import blob_convert_to_csv
@@ -8,9 +10,11 @@ from azure.storage.blob import BlobServiceClient
 
 def azure_connection(account_name: str, container_name: str):
     connection_string = ConfigConnection.get_azure_connection_string(account_name)
+
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
     container_client = blob_service_client.get_container_client(container_name)
+
     return container_client
 
 
@@ -39,37 +43,34 @@ def get_blob_data(account_name: str, container_name: str, folder_name: str):
         raise ValueError(error_message)
 
 
-# def get_kml_data(account_name: str, container_name: str, folder_name: str):
-#     try:
-#
-#         container_client = azure_connection(account_name, container_name)
-#
-#         subdirectory_path = f"{folder_name}/mission planning"
-#
-#         # List blobs in the specified subdirectory
-#         blobs = container_client.list_blobs(name_starts_with=subdirectory_path)
-#
-#         # Iterate over blobs and print those with the specified extension
-#         target_extension = ".kml"
-#         for blob in blobs:
-#             if blob.name.lower().endswith(target_extension):
-#                 blob_client = container_client.get_blob_client(blob.name)
-#                 # blob_data = blob_client.download_blob(max_concurrency=1, encoding='UTF-8')
-#                 # blob_content = blob_data.readall()  # Assuming it's a text-based file
-#                 # output_file = blob_convert_to_csv(blob_content)
-#                 blob_data = blob_client.download_blob()
-#                 content = blob_data.readall()
-#                 return content
-#
-#     except Exception as e:
-#         error_message = "An error occurred while connecting azure: " + str(e)
-#         print(error_message)
-#         raise ValueError(error_message)
-
-
-def upload_blob_to_azure(account_name: str, container_name, folder_name, local_file_path):
+def get_kml_data(account_name: str, container_name: str, folder_name: str):
     try:
 
+        container_client = azure_connection(account_name, container_name)
+
+        subdirectory_path = f"{folder_name}/mission planning"
+
+        # List blobs in the specified subdirectory
+        blobs = container_client.list_blobs(name_starts_with=subdirectory_path)
+
+        # Iterate over blobs and print those with the specified extension
+        target_extension = ".kml"
+        for blob in blobs:
+            if blob.name.lower().endswith(target_extension):
+                blob_client = container_client.get_blob_client(blob.name)
+                blob_name = os.path.basename(blob.name)
+                with open(blob_name, "wb") as local_file:
+                    blob_data = blob_client.download_blob()
+                    blob_data.readinto(local_file)
+                return blob_name
+    except Exception as e:
+        error_message = "An error occurred while connecting azure: " + str(e)
+        print(error_message)
+        raise ValueError(error_message)
+
+
+def upload_to_azure(account_name: str, container_name, folder_name, local_file_path):
+    try:
         container_client = azure_connection(account_name, container_name)
 
         destantion_blob_name = f"{folder_name}/input/{local_file_path}"
